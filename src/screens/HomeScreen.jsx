3081553a218e311877f0e90d5afa850a9837538a
Gabriel Moura <53822579+gabriel-moura2@users.noreply.github.com>
2022-08-17 22:39:18 +0000

    Commit

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,13 +4,13 @@ import { AppTitle, AppButtonLink } from '../components/';
 const HomeScreen = props => {
 	return (
 		<Container>
-			<AppTitle key="1">
+			<AppTitle>
 				Do psychological <AppButtonLink value="tests" />.
 			</AppTitle>
-			<AppTitle key="2">
+			<AppTitle>
 				Keep a <AppButtonLink value="journal" />.
 			</AppTitle>
-			<AppTitle key="3">
+			<AppTitle>
 				All our <AppButtonLink value="articles" />.
 			</AppTitle>
 		</Container>
